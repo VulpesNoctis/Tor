1e563a4ffbbd7e26ac4b5be8ad013f50b8a60bdb
Evan Graham <luaktt@gmail.com>
2016-01-25 23:23:07 +0000

    Added REQUEST logging domain
    Added REQUEST logging domain
    Changed request logging to use REQUEST domain
    Updated example torrc config

--- src/common/torlog.h
+++ src/common/torlog.h
@@ -101,8 +101,10 @@
 #define LD_SCHED     (1u<<22)
 /** Descriptors **/
 #define LD_DESCRIPTOR (1u<<23)
+/** Client Requests **/
+#define LD_REQUEST (1u<<24)
 /** Number of logging domains in the code. */
-#define N_LOGGING_DOMAINS 24
+#define N_LOGGING_DOMAINS 25
 
 /** This log message is not safe to send to a callback-based logger
  * immediately.  Used as a flag, not a log domain. */
--- src/or/directory.c
+++ src/or/directory.c
@@ -3112,13 +3112,13 @@ directory_handle_command_get(dir_connection_t *conn, const char *headers,
     if (rend_valid_descriptor_id(query)) {
       log_info(LD_REND, "Got a v2 rendezvous descriptor request for ID '%s'",
                safe_str(escaped(query)));
-	  log_notice(LD_REND, "Client request for v2 HS: DESC_ID %s", 
+	  log_notice(LD_REQUEST, "Client Request: DESC_ID %s",
 			  safe_str(query));
       switch (rend_cache_lookup_v2_desc_as_dir(query, &descp)) {
         case 1: /* valid */
 	      /* The requested desc_id was found in the rend_cache, determine the requested service_id */
 		  if(rend_desc_v2_parse_service_id(descp, service_id)>=0) {
-		      log_notice(LD_REND, "Found requested v2 HS desc: DESC_ID %s, SERVICE_ID %s", 
+		      log_notice(LD_REQUEST, "Found client request: DESC_ID %s, SERVICE_ID %s",
 					  safe_str_client(query), safe_str_client(service_id));
 		  }
           write_http_response_header(conn, strlen(descp), 0, 0);
